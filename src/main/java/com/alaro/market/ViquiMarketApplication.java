7de61c3d53b09f07b94cd01ecfd9d6dd5b096650
Viqui Alaro <viquialaro241@gmail.com>
2021-12-22 00:37:16 -0400

    implementación de controlador y se muestra una mensaje

---
+++ src/main/java/com/alaro/market/ViquiMarketApplication.java
@@ -0,0 +1,13 @@
+package com.alaro.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViquiMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViquiMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alaro/market/ViquiMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaro.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViquiMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
